ab54b4569cc6eff8048887c2cebcd0d105fec22d
Hi-nitin <nmnitin5@gmail.com>
2024-12-04 02:06:25 -0800

    first arrangement

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/home";
+
+
+const App=()=>{
+
+  return(
+    <>
+
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+
+      </Routes>
+    </BrowserRouter>
+  
+    </>
+  )
+
+}
+
+export default App;
---
+++ client/src/components/home.jsx
@@ -0,0 +1,41 @@
+import Navbar from "./navbar";
+// import {checktoken} from '../api/checktoken'
+import { useEffect, useState } from "react";
+import tokenchecker from "../api/checktoken";
+import { useNavigate } from "react-router-dom";
+import myurl from '../serverurl/url'
+const serverurl=myurl;
+const home = () => {
+    const [loading, setloading] = useState();
+    const navigate = useNavigate();
+    useEffect(() => {
+        let checkmytoken = async () => {
+            try {
+                const gettoken = await tokenchecker();
+                if (gettoken.message == 'Token is valid') {
+                    sessionStorage.setItem('id', gettoken.data)
+                    navigate('/dashboard');
+                } else {
+                    setloading(true);
+                }
+            } catch (err) {
+                alert('error getting token')
+            }
+
+        }
+        checkmytoken();
+
+    }, [])
+
+    if (!loading) {
+        return null;
+    }
+    return (
+        <>
+            <Navbar />
+            g
+        </>
+    )
+}
+
+export default home;
