3e7ac199d9c74a0de3fc3eff8727922fc58554a9
Hi-nitin <nmnitin5@gmail.com>
2024-12-09 00:48:15 -0800

    routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import Signup from "./components/signup";
 import Login from "./components/login";
+import Dashboard from './components/dashboard'
+import Myfiles from './components/myfiles'
+import Mychatroom from "./components/mychatroom";
 
 const App=()=>{
 
@@ -13,7 +16,9 @@ const App=()=>{
         <Route path="/" element={<Home />}/>
         <Route path="/signup" element={<Signup />}/>
         <Route path="/login" element={<Login/>}/>
-
+        <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/myfiles" element={<Myfiles/>}/>
+          <Route path="/mychatroom" element={<Mychatroom/>}/>
       </Routes>
     </BrowserRouter>
   
